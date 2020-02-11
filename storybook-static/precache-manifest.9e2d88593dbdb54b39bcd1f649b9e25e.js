ae809c79e1293094abe840c6a8003993df5eeb8e
lyxxxx <lyxxxx@yeah.net>
2020-02-11 21:38:21 +0800

    fix(switch): 添加依赖

---
+++ storybook-static/precache-manifest.9e2d88593dbdb54b39bcd1f649b9e25e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71dce735b244d470ac355199e16a1458",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.3bae5a273cdc700cd69b.bundle.js"
+  },
+  {
+    "url": "runtime~main.3bae5a273cdc700cd69b.bundle.js"
+  },
+  {
+    "url": "vendors~main.3bae5a273cdc700cd69b.bundle.js"
+  },
+  {
+    "url": "vendors~main.3bae5a273cdc700cd69b.bundle.js.LICENSE"
+  }
+]);
